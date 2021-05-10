c5c96ff47ee37e4319b7cebba1feaa591a5f806a
danielssen <81148474+danielssen@users.noreply.github.com>
2021-05-10 15:15:28 +0200

    Update food-categories.ts

--- src/data/food-categories.ts
+++ src/data/food-categories.ts
@@ -1,5 +1,6 @@
 export default {
-  meat: { title: "Fleischhaltig", co2: 2.18 }, 
+  redmeat: { title: "Rotes Fleisch", co2: 2.18 }, 
+  whitemeat: { title: "Weisses Fleisch", co2: 1.41 }, 
   vegetarian: { title: "Vegetarisch", co2: 0.64 },
-  vegan: { title: "Vegan", co2: 0.52 },
-}
+  vegan: { title: "Vegan", co2: 0.26 },
+} //co2 in kg pro Mahlzeit angegeben. Referenzmenge pro Mahlzeit ist die vom roten Fleisch = 2.18 kg CO2
